f4f38a3ef9811c62dc31e4b6636b611b03a71bae
Michel Daviot <michel@colisweb.com>
2019-01-28 10:41:45 +0100

    unfold implementation

--- lib/my_kata.rb
+++ lib/my_kata.rb
@@ -1,3 +1,27 @@
-def greet(who)
-  "hello, " + who
+# https://web.archive.org/web/20090327105016/http://weblog.raganwald.com/2007/11/really-useful-anamorphisms-in-ruby.html
+
+class Object
+  # unfold takes a "seed" argument and a incrementor. It returns an array. The first element of the array is the
+  # seed, every subsequentelement is the result of applying the incrementor to the previous element.
+  # so in pretentious quasi-math: result[n] = incrementor(result[n-1]).
+  # the array ends when the incrementor returns nil, so unfold(0) { |n| n+1 } is a bad idea in Ruby
+  # to add terminating conditions, use if not logic, because false does not terminate unfold.
+  #
+  # example: 10.unfold { |n| n-1 unless n == 1 }.inspect => [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+  # example: 10.class.unfold(&:superclass).inspect => [Fixnum, Integer, Numeric, Object] # using Symbol#to_proc
+  #
+  # See also: NilClass#unfold
+  def unfold(options = {}, &incrementor)
+    return [] unless options[:while].nil? || options[:while].to_proc.call(self)
+    transformed = options[:map] && options[:map].to_proc[self] || self
+    return [transformed] if options[:to] && options[:to].to_proc.call(self)
+    incrementor.call(self).unfold(options, &incrementor).unshift(transformed)
+  end
+end
+
+class NilClass
+  # See: Object#unfold
+  def unfold options = {}, &incrementor
+    []
+  end
 end
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec'
 require 'my_kata'
 
-RSpec.describe "#score" do
-  context "with no strikes or spares" do
-    it "sums the pin count for each roll" do
-      expect(greet("world")).to eq "hello, world"
-    end
+RSpec.describe "unfold" do
+
+  it "generates numbers" do
+    expect(10.unfold { |n| n - 1 unless n == 1 }).to eq [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    # expect(1.unfold (&'_+1 unless _==10')).to eq [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
   end
- end
+end
